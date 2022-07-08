08a88bf1559898450a331e8258141cc4bce2b06e
Kaijie Chen <chen@kaijie.org>
2022-07-08 11:56:59 +0800

    Public static field in ConfigUtils should be final

--- common/src/main/java/org/apache/uniffle/common/config/ConfigUtils.java
+++ common/src/main/java/org/apache/uniffle/common/config/ConfigUtils.java
@@ -177,17 +177,17 @@ public class ConfigUtils {
     return configOptionList;
   }
 
-  public static Function<Long, Boolean> positiveLongValidator = value -> value > 0;
+  public static final Function<Long, Boolean> positiveLongValidator = value -> value > 0;
 
-  public static Function<Long, Boolean> non_negativeLongValidator = value -> value >= 0;
+  public static final Function<Long, Boolean> non_negativeLongValidator = value -> value >= 0;
 
-  public static Function<Long, Boolean> positiveIntegerValidator =
+  public static final Function<Long, Boolean> positiveIntegerValidator =
       value -> value > 0L && value <= Integer.MAX_VALUE;
 
-  public static Function<Integer, Boolean> positiveIntegerValidator2 =
+  public static final Function<Integer, Boolean> positiveIntegerValidator2 =
       value -> value > 0;
 
-  public static Function<Double, Boolean> percentageDoubleValidator =
+  public static final Function<Double, Boolean> percentageDoubleValidator =
       (Function<Double, Boolean>) value -> Double.compare(value, 100.0) <= 0 && Double.compare(value, 0.0) >= 0;
   
 }
