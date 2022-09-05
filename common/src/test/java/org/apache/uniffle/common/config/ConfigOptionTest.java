b50fbc2e0a76c2cc77fce3b85c930d27d1da67c5
Junfan Zhang <junfan.zhang@outlook.com>
2022-09-05 16:35:58 +0800

    fix

--- common/src/test/java/org/apache/uniffle/common/config/ConfigOptionTest.java
+++ common/src/test/java/org/apache/uniffle/common/config/ConfigOptionTest.java
@@ -82,6 +82,16 @@ public class ConfigOptionTest {
     conf = new RssBaseConf();
     conf.setString("rss.enum", "TYPE_2");
     assertEquals(TestType.TYPE_2, conf.get(enumConfigOption));
+
+    // case4: set the illegal enum val with string
+    conf = new RssBaseConf();
+    conf.setString("rss.enum", "TYPE_3");
+    try {
+      conf.get(enumConfigOption);
+      fail();
+    } catch (IllegalArgumentException e) {
+
+    }
   }
 
   @Test
