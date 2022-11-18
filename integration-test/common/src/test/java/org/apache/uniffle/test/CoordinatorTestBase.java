6e196b1463134efb5b0190675b053885e76b5916
Junfan Zhang <zuston@apache.org>
2022-11-18 17:54:28 +0800

    ffix

--- integration-test/common/src/test/java/org/apache/uniffle/test/CoordinatorTestBase.java
+++ integration-test/common/src/test/java/org/apache/uniffle/test/CoordinatorTestBase.java
@@ -37,8 +37,6 @@ public class CoordinatorTestBase extends IntegrationTestBase {
 
   @AfterEach
   public void closeClient() {
-    if (coordinatorClient != null) {
-      coordinatorClient.close();
-    }
+    CoordinatorClientFactory.getInstance().cleanupCache();
   }
 }
