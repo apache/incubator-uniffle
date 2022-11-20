bab7643004b9f638b94f127475541702a2d985bd
wuxiaolong26 <wuxiaolong26@jd.com>
2022-11-20 17:47:07 +0800

    [Improvement] Check Spark Serializer type

--- client-spark/spark3/src/main/java/org/apache/spark/shuffle/RssShuffleManager.java
+++ client-spark/spark3/src/main/java/org/apache/spark/shuffle/RssShuffleManager.java
@@ -264,6 +264,16 @@ public class RssShuffleManager implements ShuffleManager {
   // pass that ShuffleHandle to executors (getWriter/getReader)
   @Override
   public <K, V, C> ShuffleHandle registerShuffle(int shuffleId, ShuffleDependency<K, V, C> dependency) {
+    //Spark have three kinds of serializer:
+    //org.apache.spark.serializer.JavaSerializer
+    //org.apache.spark.sql.execution.UnsafeRowSerializer
+    //org.apache.spark.serializer.KryoSerializer,
+    //Only org.apache.spark.serializer.JavaSerializer don't support RelocationOfSerializedObjects.
+    //So when we find the parameters to use org.apache.spark.serializer.JavaSerializer, We should throw an exception
+    if (!dependency.serializer.supportsRelocationOfSerializedObjects) {
+      throw new IllegalArgumentException("Can't use serialized shuffle for shuffle "+ shuffleId +", because the serializer: " +
+              dependency.serializer.getClass.getName+" does not support object relocation.");
+    }
 
     if (id.get() == null) {
       id.compareAndSet(null, SparkEnv.get().conf().getAppId() + "_" + System.currentTimeMillis());
