4b85fc80c9cc91825468f925d409cb965a925df1
smallzhongfeng <zhongjingxiong@didiglobal.com>
2022-12-06 01:10:28 +0800

    pass cs

--- coordinator/src/main/java/org/apache/uniffle/coordinator/AccessManager.java
+++ coordinator/src/main/java/org/apache/uniffle/coordinator/AccessManager.java
@@ -29,8 +29,8 @@ import org.slf4j.LoggerFactory;
 import org.apache.uniffle.common.util.Constants;
 import org.apache.uniffle.common.util.RssUtils;
 import org.apache.uniffle.coordinator.access.AccessCheckResult;
-import org.apache.uniffle.coordinator.access.checker.AccessChecker;
 import org.apache.uniffle.coordinator.access.AccessInfo;
+import org.apache.uniffle.coordinator.access.checker.AccessChecker;
 import org.apache.uniffle.coordinator.metric.CoordinatorMetrics;
 
 public class AccessManager {
