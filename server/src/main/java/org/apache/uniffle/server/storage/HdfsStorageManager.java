49190d16c6c39c9a4eeb30016245e17047cdcb37
Junfan Zhang <zuston@apache.org>
2023-01-16 14:03:11 +0800

    fix again

--- server/src/main/java/org/apache/uniffle/server/storage/HdfsStorageManager.java
+++ server/src/main/java/org/apache/uniffle/server/storage/HdfsStorageManager.java
@@ -85,31 +85,31 @@ public class HdfsStorageManager extends SingleStorageManager {
   public void removeResources(PurgeEvent event) {
     String appId = event.getAppId();
     HdfsStorage storage = getStorageByAppId(appId);
-    if (storage == null) {
-      LOG.warn("Storage gotten is null when removing resources for event: {}", event);
-      return;
-    }
-    if (event instanceof AppPurgeEvent) {
-      storage.removeHandlers(appId);
-      appIdToStorages.remove(appId);
-    }
-    ShuffleDeleteHandler deleteHandler = ShuffleHandlerFactory
-        .getInstance()
-        .createShuffleDeleteHandler(
-            new CreateShuffleDeleteHandlerRequest(StorageType.HDFS.name(), storage.getConf())
-        );
-
-    String basicPath = ShuffleStorageUtils.getFullShuffleDataFolder(storage.getStoragePath(), appId);
-    List<String> deletePaths = new ArrayList<>();
-
-    if (event instanceof AppPurgeEvent) {
-      deletePaths.add(basicPath);
-    } else {
-      for (Integer shuffleId : event.getShuffleIds()) {
-        deletePaths.add(ShuffleStorageUtils.getFullShuffleDataFolder(basicPath, String.valueOf(shuffleId)));
+    if (storage != null) {
+      if (event instanceof AppPurgeEvent) {
+        storage.removeHandlers(appId);
+        appIdToStorages.remove(appId);
       }
+      ShuffleDeleteHandler deleteHandler = ShuffleHandlerFactory
+          .getInstance()
+          .createShuffleDeleteHandler(
+              new CreateShuffleDeleteHandlerRequest(StorageType.HDFS.name(), storage.getConf())
+          );
+
+      String basicPath = ShuffleStorageUtils.getFullShuffleDataFolder(storage.getStoragePath(), appId);
+      List<String> deletePaths = new ArrayList<>();
+
+      if (event instanceof AppPurgeEvent) {
+        deletePaths.add(basicPath);
+      } else {
+        for (Integer shuffleId : event.getShuffleIds()) {
+          deletePaths.add(ShuffleStorageUtils.getFullShuffleDataFolder(basicPath, String.valueOf(shuffleId)));
+        }
+      }
+      deleteHandler.delete(deletePaths.toArray(new String[0]), appId, event.getUser());
+    } else {
+      LOG.warn("Storage gotten is null when removing resources for event: {}", event);
     }
-    deleteHandler.delete(deletePaths.toArray(new String[0]), appId, event.getUser());
   }
 
   @Override
