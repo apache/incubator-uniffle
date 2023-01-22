116125c0e4b8f4d8da46a9d84a5f273681204134
Junfan Zhang <zuston@apache.org>
2023-01-22 13:21:38 +0800

    [ISSUE-378][HugePartition][Part-3] Introduce more metrics about huge partition (#494)
    ### What changes were proposed in this pull request?
    Introduce more metrics about huge partition  
    __counter__  
    1. total_require_buffer_failed_for_huge_partition
    2. total_require_buffer_failed_for_regular_partition
    3. total_app_num
    4. total_app_with_huge_partition_num
    5. total_partition_num
    6. total_huge_partition_num
    
    __Gauge__  
    1. huge_partition_num 
    2. app_with_huge_partition_num
    
    ### Why are the changes needed?
    Having these metrics, we should observe the concrete influence from huge partition for regular partition and huge partition number in one shuffle-server
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    1. UTs

--- server/src/test/java/org/apache/uniffle/server/ShuffleTaskInfoTest.java
+++ server/src/test/java/org/apache/uniffle/server/ShuffleTaskInfoTest.java
@@ -17,15 +17,103 @@
 
 package org.apache.uniffle.server;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ShuffleTaskInfoTest {
 
+  @BeforeEach
+  public void setup() {
+    ShuffleServerMetrics.register();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    ShuffleServerMetrics.clear();
+  }
+
+  @Test
+  public void hugePartitionConcurrentTest() throws InterruptedException {
+    ShuffleTaskInfo shuffleTaskInfo = new ShuffleTaskInfo("hugePartitionConcurrentTest_appId");
+
+    // case1
+    int n = 10;
+    final CyclicBarrier barrier = new CyclicBarrier(n);
+    final CountDownLatch countDownLatch = new CountDownLatch(n);
+    ExecutorService executorService = Executors.newFixedThreadPool(n);
+    IntStream.range(0, n).forEach(i -> executorService.submit(() -> {
+      try {
+        barrier.await();
+        shuffleTaskInfo.markHugePartition(i, i);
+      } catch (Exception e) {
+        // ignore
+      } finally {
+        countDownLatch.countDown();
+      }
+    }));
+    countDownLatch.await();
+    assertEquals(1, ShuffleServerMetrics.counterTotalAppWithHugePartitionNum.get());
+    assertEquals(1, ShuffleServerMetrics.gaugeAppWithHugePartitionNum.get());
+    assertEquals(n, ShuffleServerMetrics.counterTotalHugePartitionNum.get());
+    assertEquals(n, ShuffleServerMetrics.gaugeHugePartitionNum.get());
+
+    // case2
+    ShuffleServerMetrics.clear();
+    ShuffleServerMetrics.register();
+    barrier.reset();
+    CountDownLatch latch = new CountDownLatch(n);
+    ShuffleTaskInfo taskInfo = new ShuffleTaskInfo("hugePartitionConcurrentTest_appId");
+    IntStream.range(0, n).forEach(i -> executorService.submit(() -> {
+      try {
+        barrier.await();
+        taskInfo.markHugePartition(1, 1);
+      } catch (Exception e) {
+        // ignore
+      } finally {
+        latch.countDown();
+      }
+    }));
+    latch.await();
+    assertEquals(1, ShuffleServerMetrics.counterTotalAppWithHugePartitionNum.get());
+    assertEquals(1, ShuffleServerMetrics.gaugeAppWithHugePartitionNum.get());
+    assertEquals(1, ShuffleServerMetrics.counterTotalHugePartitionNum.get());
+    assertEquals(1, ShuffleServerMetrics.gaugeHugePartitionNum.get());
+
+    executorService.shutdownNow();
+  }
+
+  @Test
+  public void hugePartitionTest() {
+    ShuffleTaskInfo shuffleTaskInfo = new ShuffleTaskInfo("hugePartition_appId");
+
+    // case1
+    assertFalse(shuffleTaskInfo.hasHugePartition());
+    assertEquals(0, shuffleTaskInfo.getHugePartitionSize());
+
+    // case2
+    shuffleTaskInfo.markHugePartition(1, 1);
+    shuffleTaskInfo.markHugePartition(1, 2);
+    shuffleTaskInfo.markHugePartition(2, 2);
+    assertTrue(shuffleTaskInfo.hasHugePartition());
+    assertEquals(3, shuffleTaskInfo.getHugePartitionSize());
+    assertEquals(1, ShuffleServerMetrics.gaugeAppWithHugePartitionNum.get());
+    assertEquals(1, ShuffleServerMetrics.counterTotalAppWithHugePartitionNum.get());
+  }
+
   @Test
   public void partitionSizeSummaryTest() {
-    ShuffleTaskInfo shuffleTaskInfo = new ShuffleTaskInfo();
+    ShuffleTaskInfo shuffleTaskInfo = new ShuffleTaskInfo("partitionSizeSummaryTest_appId");
     // case1
     long size = shuffleTaskInfo.getPartitionDataSize(0, 0);
     assertEquals(0, size);
