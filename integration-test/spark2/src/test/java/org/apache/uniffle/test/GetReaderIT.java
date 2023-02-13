32b1345fcf7aa9970c396e6f236456550a10023c
Kaijie Chen <ckj@apache.org>
2023-02-14 00:41:14 +0800

    Rename spark2 integration tests

--- integration-test/spark2/src/test/java/org/apache/uniffle/test/GetReaderIT.java
+++ integration-test/spark2/src/test/java/org/apache/uniffle/test/GetReaderIT.java
@@ -49,7 +49,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class GetReaderTest extends IntegrationTestBase {
+public class GetReaderIT extends IntegrationTestBase {
 
   @Test
   public void test() throws Exception {
