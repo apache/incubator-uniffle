b5295473bcd64098f47a528dd61d28bb251ef7df
roryqi <roryqi@tencent.com>
2023-03-07 15:04:22 +0800

    [MINOR] improvement: Reduce the Spark patch dependency

--- client-spark/spark2/src/main/java/org/apache/spark/shuffle/RssShuffleManager.java
+++ client-spark/spark2/src/main/java/org/apache/spark/shuffle/RssShuffleManager.java
@@ -187,7 +187,8 @@ public class RssShuffleManager implements ShuffleManager {
     RssSparkShuffleUtils.validateRssClientConf(sparkConf);
     // External shuffle service is not supported when using remote shuffle service
     sparkConf.set("spark.shuffle.service.enabled", "false");
-    LOG.info("Disable external shuffle service in RssShuffleManager.");
+    sparkConf.set("spark.shuffle.reduceLocality.enabled", "false");
+    LOG.info("Disable external shuffle service and shuffle data locality in RssShuffleManager.");
     if (!sparkConf.getBoolean(RssSparkConfig.RSS_TEST_FLAG.key(), false)) {
       // for non-driver executor, start a thread for sending shuffle data to shuffle server
       LOG.info("RSS data send thread is starting");
--- client-spark/spark3/src/main/java/org/apache/spark/shuffle/RssShuffleManager.java
+++ client-spark/spark3/src/main/java/org/apache/spark/shuffle/RssShuffleManager.java
@@ -202,7 +202,8 @@ public class RssShuffleManager implements ShuffleManager {
     RssSparkShuffleUtils.validateRssClientConf(sparkConf);
     // External shuffle service is not supported when using remote shuffle service
     sparkConf.set("spark.shuffle.service.enabled", "false");
-    LOG.info("Disable external shuffle service in RssShuffleManager.");
+    sparkConf.set("spark.shuffle.reduceLocality.enabled", "false");
+    LOG.info("Disable external shuffle service and shuffle data locality in RssShuffleManager.");
     sparkConf.set("spark.sql.adaptive.localShuffleReader.enabled", "false");
     LOG.info("Disable local shuffle reader in RssShuffleManager.");
     taskToSuccessBlockIds = Maps.newConcurrentMap();
