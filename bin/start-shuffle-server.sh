1e0c254ac060714a9fc0f234ebd00f616c2c2b71
Junfan Zhang <zuston@apache.org>
2023-03-08 17:50:08 +0800

    [#585] followup: fix the unbound Variable errors in start-shuffle-server.sh

--- bin/start-shuffle-server.sh
+++ bin/start-shuffle-server.sh
@@ -67,7 +67,7 @@ JAVA_LIB_PATH="-Djava.library.path=$HADOOP_HOME/lib/native"
 echo "class path is $CLASSPATH"
 
 MAX_DIRECT_MEMORY_OPTS=""
-if [ -n "$MAX_DIRECT_MEMORY_SIZE" ]; then
+if [ -n "${MAX_DIRECT_MEMORY_SIZE:-}" ]; then
   MAX_DIRECT_MEMORY_OPTS="-XX:MaxDirectMemorySize=$MAX_DIRECT_MEMORY_SIZE"
 fi
 
