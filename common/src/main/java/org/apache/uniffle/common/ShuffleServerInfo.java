17aa9afbeeb8fc91d70cfb0775921ef3b079cc6b
roryqi <roryqi@tencent.com>
2023-03-11 19:21:12 +0800

    fix codestyle

--- common/src/main/java/org/apache/uniffle/common/ShuffleServerInfo.java
+++ common/src/main/java/org/apache/uniffle/common/ShuffleServerInfo.java
@@ -86,7 +86,7 @@ public class ShuffleServerInfo implements Serializable {
 
   @Override
   public String toString() {
-    if (nettyPort > 0 ) {
+    if (nettyPort > 0) {
       return "ShuffleServerInfo{id[" + id + "], host[" + host + "],"
           + " grpc port[" + grpcPort + "], netty port[" + nettyPort + "]}";
     } else {
