4f4e4a92fafac7e0159adfd4fe5cd405d293746d
leixianming <leixianming@didiglobal.com>
2023-03-14 20:14:45 +0800

    Fix.

--- server/src/main/java/org/apache/uniffle/server/netty/decoder/StreamServerInitDecoder.java
+++ server/src/main/java/org/apache/uniffle/server/netty/decoder/StreamServerInitDecoder.java
@@ -22,10 +22,6 @@ import java.util.List;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.apache.uniffle.server.ShuffleServer;
 
 public class StreamServerInitDecoder extends ByteToMessageDecoder {
 
