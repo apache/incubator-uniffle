2be4fd737e962f4a9a221881baa396710a050b66
slfan1989 <55643692+slfan1989@users.noreply.github.com>
2023-03-15 22:17:31 +0800

    [#625] improvement: Package sun.security.krb5 is not visible in Java 11 and 17. (#726)
    ### What changes were proposed in this pull request?
    
    Try remove `sun.security.krb5.Config.refresh();`
    
    ### Why are the changes needed?
    
    `sun.security.krb5` is not visible in Java 11 and 17. We need to compile on JDK11 and JDK17
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Unit test verification.
    
    Co-authored-by: slfan1989 <louj1988@@>

--- common/src/main/java/org/apache/uniffle/common/security/HadoopSecurityContext.java
+++ common/src/main/java/org/apache/uniffle/common/security/HadoopSecurityContext.java
@@ -56,7 +56,6 @@ public class HadoopSecurityContext implements SecurityContext {
 
     if (StringUtils.isNotEmpty(krb5ConfPath)) {
       System.setProperty(KRB5_CONF_KEY, krb5ConfPath);
-      sun.security.krb5.Config.refresh();
     }
 
     Configuration conf = new Configuration(false);
