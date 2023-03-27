ea9aef4ba1973db8c8608359b511ed17d895f670
jokercurry <982458633@qq.com>
2023-03-27 19:23:04 +0800

    fix

--- client-spark/spark2/src/main/java/org/apache/spark/shuffle/RssShuffleManager.java
+++ client-spark/spark2/src/main/java/org/apache/spark/shuffle/RssShuffleManager.java
@@ -21,7 +21,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
