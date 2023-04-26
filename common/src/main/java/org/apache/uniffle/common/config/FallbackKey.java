3161b80e9d73abe217380517255baeed69ac5475
Junfan Zhang <zuston@apache.org>
2023-04-26 18:29:35 +0800

    fix

--- common/src/main/java/org/apache/uniffle/common/config/FallbackKey.java
+++ common/src/main/java/org/apache/uniffle/common/config/FallbackKey.java
@@ -52,25 +52,6 @@ public class FallbackKey {
     this.isDeprecated = isDeprecated;
   }
 
-  // ------------------------------------------------------------------------
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    } else if (o != null && o.getClass() == FallbackKey.class) {
-      FallbackKey that = (FallbackKey) o;
-      return this.key.equals(that.key) && (this.isDeprecated == that.isDeprecated);
-    } else {
-      return false;
-    }
-  }
-
-  @Override
-  public int hashCode() {
-    return 31 * key.hashCode() + (isDeprecated ? 1 : 0);
-  }
-
   @Override
   public String toString() {
     return String.format("{key=%s, isDeprecated=%s}", key, isDeprecated);
