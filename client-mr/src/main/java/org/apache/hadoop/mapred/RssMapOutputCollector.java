8904ad0e760569f7910c2ec7dc560c3aa82a81a6
zhaobing <zhaobing@zhihu.com>
2023-05-15 15:39:32 +0800

    Close shuffleclient to avoid resolving container without exiting

--- client-mr/src/main/java/org/apache/hadoop/mapred/RssMapOutputCollector.java
+++ client-mr/src/main/java/org/apache/hadoop/mapred/RssMapOutputCollector.java
@@ -33,6 +33,7 @@ import org.apache.hadoop.mapreduce.RssMRConfig;
 import org.apache.hadoop.mapreduce.RssMRUtils;
 import org.apache.hadoop.mapreduce.TaskCounter;
 import org.apache.hadoop.yarn.api.records.ApplicationAttemptId;
+import org.apache.uniffle.client.api.ShuffleWriteClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,6 +54,7 @@ public class RssMapOutputCollector<K extends Object, V extends Object>
   private int partitions;
   private SortWriteBufferManager bufferManager;
 
+  private ShuffleWriteClient shuffleClient;
   @Override
   public void init(Context context) throws IOException, ClassNotFoundException {
     JobConf mrJobConf = context.getJobConf();
@@ -107,6 +109,7 @@ public class RssMapOutputCollector<K extends Object, V extends Object>
         RssMRConfig.RSS_CLIENT_DEFAULT_SEND_THREAD_NUM);
     long maxBufferSize = RssMRUtils.getLong(rssJobConf, mrJobConf, RssMRConfig.RSS_WRITER_BUFFER_SIZE,
         RssMRConfig.RSS_WRITER_BUFFER_SIZE_DEFAULT_VALUE);
+    shuffleClient = RssMRUtils.createShuffleClient(mrJobConf);
     bufferManager = new SortWriteBufferManager(
         (long)(ByteUnit.MiB.toBytes(sortmb) * sortThreshold),
         taskAttemptId,
@@ -116,7 +119,7 @@ public class RssMapOutputCollector<K extends Object, V extends Object>
         comparator,
         memoryThreshold,
         appId,
-        RssMRUtils.createShuffleClient(mrJobConf),
+        shuffleClient,
         sendCheckInterval,
         sendCheckTimeout,
         partitionToServers,
@@ -188,6 +191,7 @@ public class RssMapOutputCollector<K extends Object, V extends Object>
   public void close() throws IOException, InterruptedException {
     reporter.progress();
     bufferManager.freeAllResources();
+    shuffleClient.close();
   }
 
   @Override
