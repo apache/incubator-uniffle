19f9566817cd8b9e7c110a9080328a176206a49a
Qing <1169158401@qq.com>
2023-05-19 16:23:11 +0800

    [#872] feat(tez): Add the common and utils class  (#890)
    ### What changes were proposed in this pull request?
    
    Tez Shuffle Read supporting Remote Shuffle related common and utils
    
    
    ### Why are the changes needed?
    
    Fix:  https://github.com/apache/incubator-uniffle/issues/872
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    UT.

---
+++ client-tez/src/test/java/org/apache/tez/common/RssTezUtilsTest.java
@@ -0,0 +1,160 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tez.common;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.tez.dag.records.TezDAGID;
+import org.apache.tez.dag.records.TezTaskAttemptID;
+import org.apache.tez.dag.records.TezTaskID;
+import org.apache.tez.dag.records.TezVertexID;
+import org.apache.uniffle.common.exception.RssException;
+import org.apache.uniffle.common.util.Constants;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class RssTezUtilsTest {
+
+  private static final String EXPECTED_EXCEPTION_MESSAGE = "Exception should be thrown";
+
+  @Test
+  public void baskAttemptIdTest() {
+    long taskAttemptId = 0x1000ad12;
+    ApplicationId appId = ApplicationId.newInstance(9999, 72);
+    TezDAGID dagId = TezDAGID.getInstance(appId, 1);
+    TezVertexID vId = TezVertexID.getInstance(dagId, 35);
+    TezTaskID taskId = TezTaskID.getInstance(vId, (int)taskAttemptId);
+    TezTaskAttemptID tezTaskAttemptId = TezTaskAttemptID.getInstance(taskId, 3);
+
+    boolean isException = false;
+    try {
+      RssTezUtils.convertTaskAttemptIdToLong(tezTaskAttemptId, 1);
+    } catch (RssException e) {
+      isException = true;
+    }
+    assertTrue(isException);
+
+    taskId = TezTaskID.getInstance(vId, (int)(1 << 21));
+    tezTaskAttemptId = TezTaskAttemptID.getInstance(taskId, 2);
+    isException = false;
+    try {
+      RssTezUtils.convertTaskAttemptIdToLong(tezTaskAttemptId, 1);
+    } catch (RssException e) {
+      isException = true;
+    }
+    assertTrue(isException);
+  }
+
+  @Test
+  public void blockConvertTest() {
+    ApplicationId appId = ApplicationId.newInstance(9999, 72);
+    TezDAGID dagId = TezDAGID.getInstance(appId, 1);
+    TezVertexID vId = TezVertexID.getInstance(dagId, 35);
+    TezTaskID tId = TezTaskID.getInstance(vId, 389);
+    TezTaskAttemptID tezTaskAttemptId = TezTaskAttemptID.getInstance(tId, 2);
+    long taskAttemptId = RssTezUtils.convertTaskAttemptIdToLong(tezTaskAttemptId, 1);
+    long blockId = RssTezUtils.getBlockId(1, taskAttemptId, 0);
+    long newTaskAttemptId = RssTezUtils.getTaskAttemptId(blockId);
+    assertEquals(taskAttemptId, newTaskAttemptId);
+    blockId = RssTezUtils.getBlockId(2, taskAttemptId, 2);
+    newTaskAttemptId = RssTezUtils.getTaskAttemptId(blockId);
+    assertEquals(taskAttemptId, newTaskAttemptId);
+  }
+
+  @Test
+  public void testPartitionIdConvertBlock() {
+    ApplicationId appId = ApplicationId.newInstance(9999, 72);
+    TezDAGID dagId = TezDAGID.getInstance(appId, 1);
+    TezVertexID vId = TezVertexID.getInstance(dagId, 35);
+    TezTaskID tId = TezTaskID.getInstance(vId, 389);
+    TezTaskAttemptID tezTaskAttemptId = TezTaskAttemptID.getInstance(tId, 2);
+    long taskAttemptId = RssTezUtils.convertTaskAttemptIdToLong(tezTaskAttemptId, 1);
+    long mask = (1L << Constants.PARTITION_ID_MAX_LENGTH) - 1;
+    for (int partitionId = 0; partitionId <= 3000; partitionId++) {
+      for (int seqNo = 0; seqNo <= 10; seqNo++) {
+        long blockId = RssTezUtils.getBlockId(Long.valueOf(partitionId), taskAttemptId, seqNo);
+        int newPartitionId = Math.toIntExact((blockId >> Constants.TASK_ATTEMPT_ID_MAX_LENGTH) & mask);
+        assertEquals(partitionId, newPartitionId);
+      }
+    }
+  }
+
+  @Test
+  public void testEstimateTaskConcurrency() {
+    Configuration jobConf = new Configuration();
+    int mapNum = 500;
+    int reduceNum = 20;
+    assertEquals(495, RssTezUtils.estimateTaskConcurrency(jobConf, mapNum, reduceNum));
+
+    jobConf.setDouble(Constants.MR_SLOW_START, 1.0);
+    assertEquals(500, RssTezUtils.estimateTaskConcurrency(jobConf, mapNum, reduceNum));
+    jobConf.setInt(Constants.MR_MAP_LIMIT, 200);
+    jobConf.setInt(Constants.MR_REDUCE_LIMIT, 200);
+    assertEquals(200, RssTezUtils.estimateTaskConcurrency(jobConf, mapNum, reduceNum));
+
+    jobConf.setDouble("mapreduce.rss.estimate.task.concurrency.dynamic.factor", 0.5);
+    assertEquals(200, RssTezUtils.estimateTaskConcurrency(jobConf,mapNum, reduceNum));
+  }
+
+  @Test
+  public void testGetRequiredShuffleServerNumber() {
+    Configuration jobConf = new Configuration();
+    int mapNum = 500;
+    int reduceNum = 20;
+    jobConf.setInt(RssTezConfig.RSS_CLIENT_ASSIGNMENT_SHUFFLE_SERVER_NUMBER, 10);
+    assertEquals(10, RssTezUtils.getRequiredShuffleServerNumber(jobConf, mapNum, reduceNum));
+    jobConf.setBoolean(RssTezConfig.RSS_ESTIMATE_SERVER_ASSIGNMENT_ENABLED, true);
+    assertEquals(10, RssTezUtils.getRequiredShuffleServerNumber(jobConf, mapNum, reduceNum));
+    jobConf.unset(RssTezConfig.RSS_CLIENT_ASSIGNMENT_SHUFFLE_SERVER_NUMBER);
+    assertEquals(7, RssTezUtils.getRequiredShuffleServerNumber(jobConf, mapNum, reduceNum));
+    jobConf.setDouble(Constants.MR_SLOW_START, 1.0);
+    assertEquals(7, RssTezUtils.getRequiredShuffleServerNumber(jobConf, mapNum, reduceNum));
+    jobConf.setInt(Constants.MR_MAP_LIMIT, 200);
+    jobConf.setInt(Constants.MR_REDUCE_LIMIT, 200);
+    assertEquals(3, RssTezUtils.getRequiredShuffleServerNumber(jobConf, mapNum, reduceNum));
+    jobConf.setDouble("mapreduce.rss.estimate.task.concurrency.dynamic.factor", 0.5);
+    assertEquals(3, RssTezUtils.getRequiredShuffleServerNumber(jobConf, mapNum, reduceNum));
+  }
+
+
+  @Test
+  public void testComputeShuffleId() {
+    int dagId = 1;
+    String upVertexName = "Map 1";
+    String downVertexName = "Reducer 2";
+    assertEquals(1001602, RssTezUtils.computeShuffleId(dagId, upVertexName, downVertexName));
+  }
+
+  @Test
+  public void testTaskIdStrToTaskId() {
+    assertEquals(0, RssTezUtils.taskIdStrToTaskId("attempt_1680867852986_0012_1_01_000000_0_10003"));
+  }
+
+  @Test
+  public void attemptTaskIdTest() {
+    String tezTaskAttemptId = "attempt_1677051234358_0091_1_00_000000_0";
+    TezTaskAttemptID originalTezTaskAttemptID = TezTaskAttemptID.fromString(tezTaskAttemptId);
+    String uniqueIdentifier = String.format("%s_%05d", tezTaskAttemptId, 3);
+    String uniqueIdentifierToAttemptId = RssTezUtils.uniqueIdentifierToAttemptId(uniqueIdentifier);
+    assertEquals(tezTaskAttemptId, uniqueIdentifierToAttemptId);
+    TezTaskAttemptID tezTaskAttemptID = TezTaskAttemptID.fromString(uniqueIdentifierToAttemptId);
+    assertEquals(originalTezTaskAttemptID, tezTaskAttemptID);
+  }
+}
---
+++ client-tez/src/test/java/org/apache/tez/common/TezIdHelperTest.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.tez.common;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TezIdHelperTest {
+
+  @Test
+  public void TestTetTaskAttemptId(){
+    TezIdHelper tezIdHelper = new TezIdHelper();
+    assertEquals(0, tezIdHelper.getTaskAttemptId(27262976));
+    assertEquals(1, tezIdHelper.getTaskAttemptId(27262977));
+    assertEquals(0, RssTezUtils.taskIdStrToTaskId("attempt_1680867852986_0012_1_01_000000_0_10003"));
+    assertEquals(tezIdHelper.getTaskAttemptId(27262976), RssTezUtils.taskIdStrToTaskId("attempt_1680867852986_0012_1_01_000000_0_10003"));
+  }
+}
