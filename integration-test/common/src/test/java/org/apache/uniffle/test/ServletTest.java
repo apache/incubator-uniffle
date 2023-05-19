7a36eeca6be9f813fea4c4e644e35f6e8d488837
cchung100m <cchung100m@cs.ccu.edu.tw>
2023-05-19 20:28:14 +0800

    [apache#417][Improvement] Fix code style: 'object def rcurly' has incorrect indentation level 4, expected level should be one of the following: 6, 8, 10. [Indentation]

--- integration-test/common/src/test/java/org/apache/uniffle/test/ServletTest.java
+++ integration-test/common/src/test/java/org/apache/uniffle/test/ServletTest.java
@@ -90,8 +90,7 @@ public class ServletTest extends IntegrationTestBase {
   public void testNodesServlet() throws Exception {
     String content = TestUtils.httpGet(NODES_URL);
     Response<List<HashMap<String, String>>> response = objectMapper.readValue(content,
-            new TypeReference<Response<List<HashMap<String, String>>>>() {
-    });
+            new TypeReference<Response<List<HashMap<String, String>>>>() {});
     List<HashMap<String, String>> serverList = response.getData();
     assertEquals(0, response.getCode());
     assertEquals(2, serverList.size());
