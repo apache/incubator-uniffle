3a8f46d703ade53f411e61eee7d025f053661003
lifeSo <1169158401@qq.com>
2023-05-22 19:49:07 +0800

    [Shuffle Common] 添加common包下面的类

---
+++ client-tez/src/main/java/org/apache/tez/common/GetShuffleServerRequest.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.tez.common;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.tez.dag.records.TezTaskAttemptID;
+
+
+public class GetShuffleServerRequest implements Writable {
+  private TezTaskAttemptID currentTaskAttemptID;
+  private int startIndex;
+  private int partitionNum;
+  private int shuffleId;
+
+  public GetShuffleServerRequest() {
+  }
+
+  public GetShuffleServerRequest(TezTaskAttemptID currentTaskAttemptID, int startIndex,
+                                 int partitionNum, int shuffleId) {
+    this.currentTaskAttemptID = currentTaskAttemptID;
+    this.startIndex = startIndex;
+    this.partitionNum = partitionNum;
+    this.shuffleId = shuffleId;
+  }
+
+  @Override
+  public void write(DataOutput output) throws IOException {
+    output.writeInt(startIndex);
+    output.writeInt(partitionNum);
+    output.writeInt(shuffleId);
+    if (currentTaskAttemptID != null) {
+      output.writeBoolean(true);
+      currentTaskAttemptID.write(output);
+    } else {
+      output.writeBoolean(false);
+    }
+  }
+
+  @Override
+  public void readFields(DataInput dataInput) throws IOException {
+    startIndex = dataInput.readInt();
+    partitionNum = dataInput.readInt();
+    shuffleId = dataInput.readInt();
+    boolean hasTaskTaskAttemptID = dataInput.readBoolean();
+    if (hasTaskTaskAttemptID) {
+      currentTaskAttemptID = new TezTaskAttemptID();
+      currentTaskAttemptID.readFields(dataInput);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "GetShuffleServerRequest{"
+            + "currentTaskAttemptID="
+            + currentTaskAttemptID
+            + ", startIndex=" + startIndex
+            + ", partitionNum=" + partitionNum
+            + ", shuffleId=" + shuffleId
+            + '}';
+  }
+
+  public TezTaskAttemptID getCurrentTaskAttemptID() {
+    return currentTaskAttemptID;
+  }
+
+  public int getStartIndex() {
+    return startIndex;
+  }
+
+  public int getPartitionNum() {
+    return partitionNum;
+  }
+
+  public int getShuffleId() {
+    return shuffleId;
+  }
+}
---
+++ client-tez/src/main/java/org/apache/tez/common/GetShuffleServerResponse.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.tez.common;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Objects;
+
+import org.apache.hadoop.io.Writable;
+
+public class GetShuffleServerResponse implements Writable {
+  private int status;
+  private String retMsg;
+  private ShuffleAssignmentsInfoWritable shuffleAssignmentsInfoWritable;
+
+  public int getStatus() {
+    return status;
+  }
+
+  public void setStatus(int status) {
+    this.status = status;
+  }
+
+  public String getRetMsg() {
+    return retMsg;
+  }
+
+  public void setRetMsg(String retMsg) {
+    this.retMsg = retMsg;
+  }
+
+  public ShuffleAssignmentsInfoWritable getShuffleAssignmentsInfoWritable() {
+    return shuffleAssignmentsInfoWritable;
+  }
+
+  public void setShuffleAssignmentsInfoWritable(ShuffleAssignmentsInfoWritable shuffleAssignmentsInfoWritable) {
+    this.shuffleAssignmentsInfoWritable = shuffleAssignmentsInfoWritable;
+  }
+
+  @Override
+  public void write(DataOutput dataOutput) throws IOException {
+    dataOutput.writeInt(status);
+    dataOutput.writeUTF(retMsg);
+    if (Objects.isNull(shuffleAssignmentsInfoWritable)) {
+      dataOutput.writeInt(-1);
+    } else {
+      dataOutput.writeInt(1);
+      shuffleAssignmentsInfoWritable.write(dataOutput);
+    }
+  }
+
+  @Override
+  public void readFields(DataInput dataInput) throws IOException {
+    status = dataInput.readInt();
+    retMsg = dataInput.readUTF();
+    shuffleAssignmentsInfoWritable = new ShuffleAssignmentsInfoWritable();
+    if (dataInput.readInt() != -1) {
+      shuffleAssignmentsInfoWritable.readFields(dataInput);
+    }
+  }
+}
