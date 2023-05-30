1189580bc478cf4429698246b628868e5d85f319
zhengchenyu <zhengchenyu16@163.com>
2023-05-30 12:07:32 +0800

    [MINOR] refactor: Fix tez workflow and checkstyle (#911)
    ### What changes were proposed in this pull request?
    
    Fix tez workflow
    
    ### Why are the changes needed?
    
    For now, workflow does not build tez client.

--- client-tez/src/test/java/org/apache/tez/common/TezIdHelperTest.java
+++ client-tez/src/test/java/org/apache/tez/common/TezIdHelperTest.java
@@ -18,16 +18,18 @@
 package org.apache.tez.common;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TezIdHelperTest {
 
   @Test
-  public void TestTetTaskAttemptId(){
+  public void testTetTaskAttemptId() {
     TezIdHelper tezIdHelper = new TezIdHelper();
     assertEquals(0, tezIdHelper.getTaskAttemptId(27262976));
     assertEquals(1, tezIdHelper.getTaskAttemptId(27262977));
     assertEquals(0, RssTezUtils.taskIdStrToTaskId("attempt_1680867852986_0012_1_01_000000_0_10003"));
-    assertEquals(tezIdHelper.getTaskAttemptId(27262976), RssTezUtils.taskIdStrToTaskId("attempt_1680867852986_0012_1_01_000000_0_10003"));
+    assertEquals(tezIdHelper.getTaskAttemptId(27262976),
+        RssTezUtils.taskIdStrToTaskId("attempt_1680867852986_0012_1_01_000000_0_10003"));
   }
 }
