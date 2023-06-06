604946a1f0adc2ea5dfbe4ee9dedb4a82b6ac722
lifeSo <1169158401@qq.com>
2023-06-07 00:48:00 +0800

    modify

--- client-tez/src/test/java/org/apache/tez/runtime/library/common/shuffle/impl/RssSimpleFetchedInputAllocatorTest.java
+++ client-tez/src/test/java/org/apache/tez/runtime/library/common/shuffle/impl/RssSimpleFetchedInputAllocatorTest.java
@@ -38,17 +38,17 @@ public class RssSimpleFetchedInputAllocatorTest {
   
   @Test
   public void testAllocate() throws IOException {
+    String localDirs = "/tmp/" + this.getClass().getName();
     Configuration conf = new Configuration();
-    
-    long jvmMax = 954728448;
+
+    long jvmMax = 954728448L;
     LOG.info("jvmMax: " + jvmMax);
-    
+
     float bufferPercent = 0.1f;
     conf.setFloat(TezRuntimeConfiguration.TEZ_RUNTIME_SHUFFLE_FETCH_BUFFER_PERCENT, bufferPercent);
     conf.setFloat(TezRuntimeConfiguration.TEZ_RUNTIME_SHUFFLE_MEMORY_LIMIT_PERCENT, 1.0f);
-    String localDirs = "/tmp/" + this.getClass().getName();
     conf.setStrings(TezRuntimeFrameworkConfigs.LOCAL_DIRS, localDirs);
-    
+
     long inMemThreshold = (long) (bufferPercent * jvmMax);
     LOG.info("InMemThreshold: " + inMemThreshold);
 
@@ -59,24 +59,29 @@ public class RssSimpleFetchedInputAllocatorTest {
     long requestSize = (long) (0.4f * inMemThreshold);
     long compressedSize = 1L;
     LOG.info("RequestSize: " + requestSize);
-    
+
     FetchedInput fi1 = inputManager.allocate(requestSize, compressedSize, new InputAttemptIdentifier(1, 1));
     assertEquals(FetchedInput.Type.MEMORY, fi1.getType());
-    
+
+    FetchedInput fi2 = inputManager.allocate(requestSize, compressedSize, new InputAttemptIdentifier(2, 1));
+    assertEquals(FetchedInput.Type.MEMORY, fi2.getType());
+
     // Over limit by this point. Next reserve should give back a DISK allocation
     FetchedInput fi3 = inputManager.allocate(requestSize, compressedSize, new InputAttemptIdentifier(3, 1));
     assertEquals(FetchedInput.Type.DISK, fi3.getType());
-    
-    
+
+
     // Freed one memory allocation. Next should be mem again.
     fi1.abort();
     fi1.free();
     FetchedInput fi4 = inputManager.allocate(requestSize, compressedSize, new InputAttemptIdentifier(4, 1));
     assertEquals(FetchedInput.Type.MEMORY, fi4.getType());
-    
+
     // Freed one disk allocation. Next sould be disk again (no mem freed)
     fi3.abort();
     fi3.free();
+    FetchedInput fi5 = inputManager.allocate(requestSize, compressedSize, new InputAttemptIdentifier(4, 1));
+    assertEquals(FetchedInput.Type.DISK, fi5.getType());
   }
 }
 
