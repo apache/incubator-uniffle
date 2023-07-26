10aa4b1f8fa41973de60c9adfc29d9a76a3488e3
leixianming <leixianming@didiglobal.com>
2023-07-26 13:18:21 +0800

    Fix.

--- coordinator/src/test/java/org/apache/uniffle/coordinator/strategy/assignment/PartitionBalanceAssignmentStrategyTest.java
+++ coordinator/src/test/java/org/apache/uniffle/coordinator/strategy/assignment/PartitionBalanceAssignmentStrategyTest.java
@@ -55,8 +55,8 @@ public class PartitionBalanceAssignmentStrategyTest {
   public void setUp() throws Exception {
     CoordinatorConf ssc = new CoordinatorConf();
     ssc.set(
-            CoordinatorConf.COORDINATOR_SELECT_PARTITION_STRATEGY,
-            AbstractAssignmentStrategy.SelectPartitionStrategyName.ROUND);
+        CoordinatorConf.COORDINATOR_SELECT_PARTITION_STRATEGY,
+        AbstractAssignmentStrategy.SelectPartitionStrategyName.ROUND);
     ssc.setInteger(CoordinatorConf.COORDINATOR_SHUFFLE_NODES_MAX, shuffleNodesMax);
     clusterManager = new SimpleClusterManager(ssc, new Configuration());
     strategy = new PartitionBalanceAssignmentStrategy(clusterManager, ssc);
