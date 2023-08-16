a7c8e59ab2019f74742713b82152e22e289276e6
Junfan Zhang <zuston@apache.org>
2023-08-16 14:38:55 +0800

    fix(netty): incorrect bytebuf release for ShuffleBlockInfo data in client side

--- client-spark/common/src/main/java/org/apache/spark/shuffle/writer/WriteBufferManager.java
+++ client-spark/common/src/main/java/org/apache/spark/shuffle/writer/WriteBufferManager.java
@@ -409,9 +409,15 @@ public class WriteBufferManager extends MemoryConsumer {
                 + " bytes");
         // Use final temporary variables for closures
         final long _memoryUsed = memoryUsed;
+        List<ShuffleBlockInfo> _blocks = shuffleBlockInfosPerEvent;
         events.add(
             new AddBlockEvent(
-                taskId, shuffleBlockInfosPerEvent, () -> freeAllocatedMemory(_memoryUsed)));
+                taskId,
+                shuffleBlockInfosPerEvent,
+                () -> {
+                  freeAllocatedMemory(_memoryUsed);
+                  _blocks.stream().forEach(x -> x.getData().release());
+                }));
         shuffleBlockInfosPerEvent = Lists.newArrayList();
         totalSize = 0;
         memoryUsed = 0;
@@ -426,16 +432,14 @@ public class WriteBufferManager extends MemoryConsumer {
               + " bytes");
       // Use final temporary variables for closures
       final long _memoryUsed = memoryUsed;
-      final List<ShuffleBlockInfo> finalShuffleBlockInfosPerEvent = shuffleBlockInfoList;
+      final List<ShuffleBlockInfo> _blocks = shuffleBlockInfosPerEvent;
       events.add(
           new AddBlockEvent(
               taskId,
               shuffleBlockInfosPerEvent,
               () -> {
                 freeAllocatedMemory(_memoryUsed);
-                for (ShuffleBlockInfo shuffleBlockInfo : finalShuffleBlockInfosPerEvent) {
-                  shuffleBlockInfo.getData().release();
-                }
+                _blocks.stream().forEach(x -> x.getData().release());
               }));
     }
     return events;
