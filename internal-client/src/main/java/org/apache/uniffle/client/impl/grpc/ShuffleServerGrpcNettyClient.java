f8a9d8cbf530fc7c5e7bb5b6f8d8a4908b32d98b
jam.xu <jam.xu@vipshop.com>
2023-08-31 15:24:06 +0800

    fix code style

--- internal-client/src/main/java/org/apache/uniffle/client/impl/grpc/ShuffleServerGrpcNettyClient.java
+++ internal-client/src/main/java/org/apache/uniffle/client/impl/grpc/ShuffleServerGrpcNettyClient.java
@@ -61,7 +61,8 @@ public class ShuffleServerGrpcNettyClient extends ShuffleServerGrpcClient {
     this(rssConf, host, grpcPort, nettyPort, 3);
   }
 
-  public ShuffleServerGrpcNettyClient(RssConf rssConf, String host, int grpcPort, int nettyPort, int maxRetryAttempts) {
+  public ShuffleServerGrpcNettyClient(
+      RssConf rssConf, String host, int grpcPort, int nettyPort, int maxRetryAttempts) {
     super(host, grpcPort, maxRetryAttempts);
     this.nettyPort = nettyPort;
     TransportContext transportContext = new TransportContext(new TransportConf(rssConf));
