29f53cf265cc31afa169d1735e1aae3f5da73d4c
yl09099 <shaq376260428@qq.com>
2023-11-10 21:18:04 +0800

    [#1309][Bug][#825][part-3] WriteBufferManager in Spark2 does not use a reassigned shuffle server.

--- client-spark/spark2/src/main/java/org/apache/spark/shuffle/writer/RssShuffleWriter.java
+++ client-spark/spark2/src/main/java/org/apache/spark/shuffle/writer/RssShuffleWriter.java
@@ -208,7 +208,7 @@ public class RssShuffleWriter<K, V, C> extends ShuffleWriter<K, V> {
             taskAttemptId,
             bufferOptions,
             rssHandle.getDependency().serializer(),
-            rssHandle.getPartitionToServers(),
+            shuffleHandleInfo.getPartitionToServers(),
             context.taskMemoryManager(),
             shuffleWriteMetrics,
             RssSparkConfig.toRssConf(sparkConf),
