b9e0024cc2e9cf5cb94eda83fc5f27ada2f8c010
leixianming <leixianming@didiglobal.com>
2023-11-15 11:38:43 +0800

    Fix.

--- common/src/main/java/org/apache/uniffle/common/netty/buffer/NettyManagedBuffer.java
+++ common/src/main/java/org/apache/uniffle/common/netty/buffer/NettyManagedBuffer.java
@@ -24,7 +24,8 @@ import io.netty.buffer.Unpooled;
 
 public class NettyManagedBuffer extends ManagedBuffer {
 
-  public final static NettyManagedBuffer EMPTY_BUFFER = new NettyManagedBuffer(Unpooled.buffer(0, 0));
+  public static final NettyManagedBuffer EMPTY_BUFFER =
+      new NettyManagedBuffer(Unpooled.buffer(0, 0));
 
   private ByteBuf buf;
 
