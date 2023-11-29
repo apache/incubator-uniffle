728b242c6cbce2c765e355e2a49a4e9b0e946ab6
Junfan Zhang <zuston@apache.org>
2023-11-29 10:05:29 +0800

    fix

--- storage/src/main/java/org/apache/uniffle/storage/common/LocalStorage.java
+++ storage/src/main/java/org/apache/uniffle/storage/common/LocalStorage.java
@@ -168,11 +168,13 @@ public class LocalStorage extends AbstractStorage {
     if (isSpaceEnough) {
       serviceUsedCapacityCheck =
           metaData.getDiskSize().doubleValue() * 100 / capacity < highWaterMarkOfWrite;
-      diskUsedCapacityCheck = ((double)(diskCapacity - diskFree)) * 100 / diskCapacity < highWaterMarkOfWrite;
+      diskUsedCapacityCheck =
+          ((double) (diskCapacity - diskFree)) * 100 / diskCapacity < highWaterMarkOfWrite;
     } else {
       serviceUsedCapacityCheck =
           metaData.getDiskSize().doubleValue() * 100 / capacity < lowWaterMarkOfWrite;
-      diskUsedCapacityCheck = ((double)(diskCapacity - diskFree)) * 100 / diskCapacity < lowWaterMarkOfWrite;
+      diskUsedCapacityCheck =
+          ((double) (diskCapacity - diskFree)) * 100 / diskCapacity < lowWaterMarkOfWrite;
     }
     isSpaceEnough =
         serviceUsedCapacityCheck && (enableDiskCapacityCheck ? diskUsedCapacityCheck : true);
