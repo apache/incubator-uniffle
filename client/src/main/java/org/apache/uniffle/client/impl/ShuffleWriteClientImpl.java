59da7d700913a055a150e07bbc13042108239ba3
wforget <643348094@qq.com>
2023-12-07 14:08:13 +0800

    fix

--- client/src/main/java/org/apache/uniffle/client/impl/ShuffleWriteClientImpl.java
+++ client/src/main/java/org/apache/uniffle/client/impl/ShuffleWriteClientImpl.java
@@ -27,7 +27,6 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
