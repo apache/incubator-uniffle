9ca43bda49d5e6ea2a5ce5685860f6236f38df1f
Junfan Zhang <zuston@apache.org>
2023-12-13 16:37:29 +0800

    [#1364] feat(client): introduce option to control whether to use local hadoop conf for remote storage by default

--- client-spark/common/src/main/java/org/apache/uniffle/shuffle/manager/RssShuffleManagerBase.java
+++ client-spark/common/src/main/java/org/apache/uniffle/shuffle/manager/RssShuffleManagerBase.java
@@ -22,17 +22,20 @@ import java.lang.reflect.Method;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import org.apache.spark.MapOutputTracker;
-import org.apache.spark.MapOutputTrackerMaster;
-import org.apache.spark.SparkEnv;
-import org.apache.spark.SparkException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.spark.*;
+import org.apache.spark.shuffle.RssSparkConfig;
 import org.apache.spark.shuffle.RssSparkShuffleUtils;
 import org.apache.spark.shuffle.ShuffleManager;
 import org.apache.spark.shuffle.SparkVersionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.apache.uniffle.common.RemoteStorageInfo;
+import org.apache.uniffle.common.config.RssConf;
 import org.apache.uniffle.common.exception.RssException;
+
+import static org.apache.uniffle.common.config.RssClientConf.RSS_CLIENT_REMOTE_STORAGE_USE_LOCAL_CONF_ENABLED;
 
 public abstract class RssShuffleManagerBase implements RssShuffleManagerInterface, ShuffleManager {
   private static final Logger LOG = LoggerFactory.getLogger(RssShuffleManagerBase.class);
@@ -150,5 +153,15 @@ public abstract class RssShuffleManagerBase implements RssShuffleManagerInterfac
     MapOutputTracker tracker =
         Optional.ofNullable(SparkEnv.get()).map(SparkEnv::mapOutputTracker).orElse(null);
     return tracker instanceof MapOutputTrackerMaster ? (MapOutputTrackerMaster) tracker : null;
+  }
+
+  protected RemoteStorageInfo getRemoteStorageInfo(SparkConf sparkConf) {
+    RssConf rssConf = RssSparkConfig.toRssConf(sparkConf);
+    if (rssConf.getBoolean(RSS_CLIENT_REMOTE_STORAGE_USE_LOCAL_CONF_ENABLED)) {
+      return new RemoteStorageInfo(
+          sparkConf.get(RssSparkConfig.RSS_REMOTE_STORAGE_PATH.key(), ""), new Configuration(true));
+    }
+
+    return new RemoteStorageInfo(sparkConf.get(RssSparkConfig.RSS_REMOTE_STORAGE_PATH.key(), ""));
   }
 }
