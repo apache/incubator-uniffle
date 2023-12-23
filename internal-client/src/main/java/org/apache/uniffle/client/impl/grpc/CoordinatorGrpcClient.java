c4f4761544fdb53e00dda1aa854b371317a70953
lifeSo <1169158401@qq.com>
2023-12-23 20:08:33 +0800

    test

--- internal-client/src/main/java/org/apache/uniffle/client/impl/grpc/CoordinatorGrpcClient.java
+++ internal-client/src/main/java/org/apache/uniffle/client/impl/grpc/CoordinatorGrpcClient.java
@@ -145,6 +145,8 @@ public class CoordinatorGrpcClient extends GrpcClient implements CoordinatorClie
       response = blockingStub.withDeadlineAfter(timeout, TimeUnit.MILLISECONDS).heartbeat(request);
       status = response.getStatus();
     } catch (StatusRuntimeException e) {
+      LOG.error("-------------StatusRuntimeException:");
+      LOG.error("StatusRuntimeException error:", e);
       LOG.error(e.getMessage());
       status = RssProtos.StatusCode.TIMEOUT;
     } catch (Exception e) {
