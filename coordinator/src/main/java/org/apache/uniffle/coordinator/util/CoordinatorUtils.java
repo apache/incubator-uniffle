21496b8deb24f9725341ac1347830a1bd186f9e0
myandpr <myanstu@163.com>
2024-01-07 13:21:21 +0800

    fix java checkstyle

--- coordinator/src/main/java/org/apache/uniffle/coordinator/util/CoordinatorUtils.java
+++ coordinator/src/main/java/org/apache/uniffle/coordinator/util/CoordinatorUtils.java
@@ -17,7 +17,9 @@
 
 package org.apache.uniffle.coordinator.util;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
