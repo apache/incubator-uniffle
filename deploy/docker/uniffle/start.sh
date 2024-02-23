573d101fad20c1bbeb5d8cdf7bf851d717f3a087
Enrico Minack <github@enrico.minack.dev>
2024-02-23 15:54:27 +0100

    Use rss container healthy state

--- deploy/docker/uniffle/start.sh
+++ deploy/docker/uniffle/start.sh
@@ -25,27 +25,17 @@ echo "coordinator_conf: $coordinator_conf"
 server_conf=$basedir'/conf/server.conf'
 echo "server_conf: $server_conf"
 
-
 if [ "$SERVICE_NAME" == "coordinator" ];then
     start_script=${basedir}'/bin/start-coordinator.sh'
     log_file=$basedir'/logs/coordinator.log'
-    rpc_port=${COORDINATOR_RPC_PORT}
 fi
 
 if [ "$SERVICE_NAME" == "server" ];then
     start_script=${basedir}'/bin/start-shuffle-server.sh'
     log_file=$basedir'/logs/shuffle_server.log'
-    rpc_port=${SERVER_RPC_PORT}
 fi
 
-(bash ${start_script} | grep -v "class path is") &
-sleep 30
-lsof=$(lsof -i:"${rpc_port}" -sTCP:LISTEN)
-if [ "$lsof" = "" ]; then
-  cat ${log_file}
-  exit 1
-fi
-
-echo "$lsof"
-hostname
-tail -f ${log_file}
+touch "${log_file}"
+${start_script} | grep -v "class path is"
+echo
+tail -n +0 -f "${log_file}"
