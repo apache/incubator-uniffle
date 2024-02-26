f4a0a300dee0f569c28d31bcb5f2c9816a6e4171
WenlongBrother <2848694359@qq.com>
2024-02-26 21:42:48 +0800

    remove duplicate dependency and use toml4j

--- integration-test/rust/src/test/java/org/apache/uniffle/test/RustShuffleServerConf.java
+++ integration-test/rust/src/test/java/org/apache/uniffle/test/RustShuffleServerConf.java
@@ -17,25 +17,18 @@
 
 package org.apache.uniffle.test;
 
+import com.moandjiezana.toml.TomlWriter;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RustShuffleServerConf {
 
-  private static final Logger LOG = LoggerFactory.getLogger(RustShuffleServerConf.class);
-
   private final Map<String, Object> conf;
-
   private final File tempDir;
-
   private final File tempFile;
 
   public RustShuffleServerConf(File tempDir) {
@@ -57,78 +50,9 @@ public class RustShuffleServerConf {
   }
 
   public void generateTomlConf() throws IOException {
-    writeToFile(toTomlString(conf), tempFile.getPath());
-  }
-
-  public String toTomlString(Map<String, Object> tomlData) {
-    StringBuilder tomlBuilder = new StringBuilder();
-
-    for (Map.Entry<String, Object> entry : tomlData.entrySet()) {
-      if (!(entry.getValue() instanceof Map)) {
-        appendEntry(tomlBuilder, entry);
-      }
-    }
-
-    for (Map.Entry<String, Object> entry : tomlData.entrySet()) {
-      if (entry.getValue() instanceof Map) {
-        tomlBuilder.append("\n[").append(entry.getKey()).append("]\n");
-        Map<?, ?> nestedMap = (Map<?, ?>) entry.getValue();
-        for (Map.Entry<?, ?> nestedEntry : nestedMap.entrySet()) {
-          appendEntry(tomlBuilder, nestedEntry);
-        }
-      }
-    }
-
-    return tomlBuilder.toString();
-  }
-
-  private void appendEntry(StringBuilder builder, Map.Entry<?, ?> entry) {
-    if (entry.getValue() instanceof List) {
-      builder
-          .append(entry.getKey())
-          .append(" = ")
-          .append(listToString((List<?>) entry.getValue()))
-          .append("\n");
-    } else {
-      builder
-          .append(entry.getKey())
-          .append(" = ")
-          .append(valueToString(entry.getValue()))
-          .append("\n");
-    }
-  }
-
-  private String mapToString(Map<?, ?> map) {
-    StringBuilder mapBuilder = new StringBuilder();
-    for (Map.Entry<?, ?> entry : map.entrySet()) {
-      mapBuilder
-          .append(entry.getKey())
-          .append(" = ")
-          .append(valueToString(entry.getValue()))
-          .append("\n");
-    }
-    return mapBuilder.toString();
-  }
-
-  private String listToString(List<?> list) {
-    StringBuilder listBuilder = new StringBuilder("[");
-    boolean first = true;
-    for (Object item : list) {
-      if (!first) {
-        listBuilder.append(", ");
-      }
-      listBuilder.append(valueToString(item));
-      first = false;
-    }
-    listBuilder.append("]");
-    return listBuilder.toString();
-  }
-
-  private String valueToString(Object value) {
-    if (value instanceof String) {
-      return "\"" + value + "\"";
-    }
-    return value.toString();
+    TomlWriter tomlWriter = new TomlWriter();
+    String toml = tomlWriter.write(conf);
+    writeToFile(toml, tempFile.getPath());
   }
 
   private void writeToFile(String content, String filePath) throws IOException {
