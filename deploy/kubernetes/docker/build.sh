a73dcb968ee3fde7170af300c4af238ab26c2de7
Enrico Minack <github@enrico.minack.dev>
2024-02-27 03:11:29 +0100

    feat(docker): Add example docker compose Uniffle/Spark cluster (#1532)
    ### What changes were proposed in this pull request?
    Adds code to spin up an example Uniffle/Spark docker cluster using docker compose. This is used by the CI to test the example cluster setup.
    
    ### Why are the changes needed?
    This setup has a smaller footprint than the existing kubernetes example in `deploy/kubernetes/integration-test/e2e/README.md`, which is not trivial to setup. The new example only requires Docker to be installed, and can be spun up via
    
        ./deploy/docker-compose/build.sh
        docker compose -f deploy/docker-compose/docker-compose.yml up
    
    The Uniffle and Spark cluster can be used to interactively test Spark with Uniffle.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Manually and CI tested.

--- deploy/kubernetes/docker/build.sh
+++ deploy/kubernetes/docker/build.sh
@@ -17,6 +17,10 @@
 # limitations under the License.
 #
 
+set -o pipefail
+set -o nounset   # exit the script if you try to use an uninitialised variable
+set -o errexit   # exit the script if any statement returns a non-true return value
+
 function exit_with_usage() {
   set +x
   echo "./build.sh - Tool for building docker images of Remote Shuffle Service"
