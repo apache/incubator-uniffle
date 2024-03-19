e3fea552e056e4ffc2a654d255f85d4ff4d54b89
QI Jiale <qijiale76@foxmail.com>
2024-03-19 15:00:05 +0800

    [FEATURE] Support Spark 3.5.1

--- integration-test/spark3/src/test/java/org/apache/uniffle/test/GetReaderTest.java
+++ integration-test/spark3/src/test/java/org/apache/uniffle/test/GetReaderTest.java
@@ -296,6 +296,12 @@ public class GetReaderTest extends IntegrationTestBase {
     public boolean isCompleted() {
       return false;
     }
+    
+    // The following method is only available after Spark 3.5.1, and in order to be compatible 
+    // with the version before Spark 3.5.1, the annotation @Override is not added.
+    public boolean isFailed() {
+      return false;
+    }
 
     @Override
     public Properties getLocalProperties() {
