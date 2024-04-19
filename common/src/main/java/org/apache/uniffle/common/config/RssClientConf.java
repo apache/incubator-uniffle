1f77cc120812bd131355e064ca81bb6b2a755fc0
rickyma <rickyma@tencent.com>
2024-04-19 15:28:04 +0800

    [#1664] Update the descriptions and default values of some configurations

--- common/src/main/java/org/apache/uniffle/common/config/RssClientConf.java
+++ common/src/main/java/org/apache/uniffle/common/config/RssClientConf.java
@@ -193,7 +193,8 @@ public class RssClientConf {
       ConfigOptions.key("rss.client.type")
           .enumType(ClientType.class)
           .defaultValue(ClientType.GRPC)
-          .withDescription("Supports GRPC, GRPC_NETTY");
+          .withDescription(
+              "Supports GRPC_NETTY, GRPC. The default value is GRPC. But we recommend using GRPC_NETTY to enable Netty on the client side for better stability and performance.");
 
   public static final ConfigOption<Boolean> RSS_CLIENT_REMOTE_STORAGE_USE_LOCAL_CONF_ENABLED =
       ConfigOptions.key("rss.client.remote.storage.useLocalConfAsDefault")
