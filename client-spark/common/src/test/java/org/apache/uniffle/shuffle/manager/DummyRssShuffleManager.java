6bf6c96f8cf8d75cdec3d471d6ab679b5c8e30f9
Junfan Zhang <zuston@apache.org>
2024-06-19 16:09:40 +0800

    fix

--- client-spark/common/src/test/java/org/apache/uniffle/shuffle/manager/DummyRssShuffleManager.java
+++ client-spark/common/src/test/java/org/apache/uniffle/shuffle/manager/DummyRssShuffleManager.java
@@ -31,6 +31,7 @@ import org.apache.uniffle.shuffle.BlockIdManager;
 
 public class DummyRssShuffleManager implements RssShuffleManagerInterface {
   public Set<Integer> unregisteredShuffleIds = new LinkedHashSet<>();
+  private RssStageResubmitManager stageResubmitManager = new RssStageResubmitManager();
 
   @Override
   public String getAppId() {
@@ -87,6 +88,6 @@ public class DummyRssShuffleManager implements RssShuffleManagerInterface {
 
   @Override
   public RssStageResubmitManager getStageResubmitManager() {
-    return null;
+    return stageResubmitManager;
   }
 }
