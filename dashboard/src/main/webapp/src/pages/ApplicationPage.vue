apache/incubator-uniffle

commit c48af78e98ad4c49be403b157a77cca75cbe06fe

[#1401] feat(dashboard): Shuffle server page list sorting function (#1868)
### What changes were proposed in this pull request?

On the Shuffle Server list page, you can sort the Shuffle Server by column to facilitate you to view information about the Shuffle Server.You can sort IP, used memory, pre-allocated memory, available memory, heartbeat time, and so on.
![image](https://github.com/apache/incubator-uniffle/assets/33595968/a9a04744-29e3-40a7-b3f1-a2586fc8dad0)

### Why are the changes needed?

Fix: #1401 

### Does this PR introduce _any_ user-facing change?

Yes.

### How was this patch tested?

Page view.

dashboard/src/main/webapp/src/pages/ApplicationPage.vue
@@ -32,28 +32,42 @@
     <el-divider />
     <el-tag>User App ranking</el-tag>
     <div>
-      <el-table :data="pageData.userAppCount" height="250" style="width: 100%">
-        <el-table-column prop="userName" label="UserName" min-width="180" />
-        <el-table-column prop="appNum" label="Totality" min-width="180" />
+      <el-table
+        :data="pageData.userAppCount"
+        height="250"
+        style="width: 100%"
+        :default-sort="sortAppCollect"
+        @sort-change="sortAppCollectChangeEvent"
+      >
+        <el-table-column prop="userName" label="UserName" min-width="180" sortable fixed />
+        <el-table-column prop="appNum" label="Totality" min-width="180" sortable />
       </el-table>
     </div>
     <el-divider />
     <el-tag>Apps</el-tag>
     <div>
-      <el-table :data="pageData.appInfoData" height="250" style="width: 100%">
-        <el-table-column prop="appId" label="AppId" min-width="180" />
-        <el-table-column prop="userName" label="UserName" min-width="180" />
-        <el-table-column
+      <el-table
+        :data="pageData.appInfoData"
+        height="250"
+        style="width: 100%"
+        :default-sort="sortApp"
+        @sort-change="sortAppChangeEvent"
+      >
+        <el-table-column prop="appId" label="AppId" min-width="180" sortable fixed />
+        <el-table-column prop="userName" label="UserName" min-width="180" sortable />
+        <el-table-columnsortable
           prop="registrationTime"
           label="Registration Time"
           min-width="180"
           :formatter="dateFormatter"
+          sortable
         />
         <el-table-column
           prop="updateTime"
           label="Update Time"
           min-width="180"
           :formatter="dateFormatter"
+          sortable
         />
       </el-table>
     </div>
@@ -107,7 +121,31 @@ export default {
         getAppTotalPage()
       }
     })
-    return { pageData, dateFormatter }
+
+    const sortAppCollect = reactive({})
+    const sortAppCollectChangeEvent = (sortInfo) => {
+      for (const sortColumnKey in sortAppCollect) {
+        delete sortAppCollect[sortColumnKey]
+      }
+      sortAppCollect[sortInfo.prop] = sortInfo.order
+    }
+
+    const sortApp = reactive({})
+    const sortAppChangeEvent = (sortInfo) => {
+      for (const sortColumnKey in sortApp) {
+        delete sortApp[sortColumnKey]
+      }
+      sortApp[sortInfo.prop] = sortInfo.order
+    }
+
+    return {
+      pageData,
+      sortAppCollect,
+      sortAppCollectChangeEvent,
+      sortApp,
+      sortAppChangeEvent,
+      dateFormatter
+    }
   }
 }
 </script>

dashboard/src/main/webapp/src/pages/ShuffleServerPage.vue
@@ -121,6 +121,7 @@ export default {
         UNHEALTHY: 0
       }
     })
+
     const currentServerStore = useCurrentServerStore()
 
     async function getShufflegetStatusTotalPage() {

dashboard/src/main/webapp/src/pages/serverstatus/ActiveNodeListPage.vue
@@ -1,106 +1,0 @@
-<!--
-  ~ Licensed to the Apache Software Foundation (ASF) under one or more
-  ~ contributor license agreements.  See the NOTICE file distributed with
-  ~ this work for additional information regarding copyright ownership.
-  ~ The ASF licenses this file to You under the Apache License, Version 2.0
-  ~ (the "License"); you may not use this file except in compliance with
-  ~ the License.  You may obtain a copy of the License at
-  ~
-  ~    http://www.apache.org/licenses/LICENSE-2.0
-  ~
-  ~ Unless required by applicable law or agreed to in writing, software
-  ~ distributed under the License is distributed on an "AS IS" BASIS,
-  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  ~ See the License for the specific language governing permissions and
-  ~ limitations under the License.
-  -->
-
-<template>
-  <div>
-    <el-table :data="pageData.tableData" height="550" style="width: 100%">
-      <el-table-column prop="id" label="Id" min-width="140" />
-      <el-table-column prop="ip" label="IP" min-width="80" />
-      <el-table-column prop="grpcPort" label="GrpcPort" min-width="80" />
-      <el-table-column prop="nettyPort" label="NettyPort" min-width="80" />
-      <el-table-column prop="usedMemory" label="UsedMem" min-width="80" :formatter="memFormatter" />
-      <el-table-column
-        prop="preAllocatedMemory"
-        label="PreAllocatedMem"
-        min-width="80"
-        :formatter="memFormatter"
-      />
-      <el-table-column
-        prop="availableMemory"
-        label="AvailableMem"
-        min-width="80"
-        :formatter="memFormatter"
-      />
-      <el-table-column prop="eventNumInFlush" label="FlushNum" min-width="80" />
-      <el-table-column prop="status" label="Status" min-width="80" />
-      <el-table-column
-        prop="registrationTime"
-        label="RegistrationTime"
-        min-width="80"
-        :formatter="dateFormatter"
-      />
-      <el-table-column
-        prop="timestamp"
-        label="HeartbeatTime"
-        min-width="80"
-        :formatter="dateFormatter"
-      />
-      <el-table-column prop="tags" label="Tags" min-width="80" />
-    </el-table>
-  </div>
-</template>
-<script>
-import { onMounted, reactive } from 'vue'
-import { getShuffleActiveNodes } from '@/api/api'
-import { memFormatter, dateFormatter } from '@/utils/common'
-import { useCurrentServerStore } from '@/store/useCurrentServerStore'
-
-export default {
-  setup() {
-    const pageData = reactive({
-      tableData: [
-        {
-          id: '',
-          ip: '',
-          grpcPort: 0,
-          nettyPort: 0,
-          usedMemory: 0,
-          preAllocatedMemory: 0,
-          availableMemory: 0,
-          eventNumInFlush: 0,
-          tags: '',
-          status: '',
-          registrationTime: '',
-          timestamp: ''
-        }
-      ]
-    })
-    const currentServerStore = useCurrentServerStore()
-
-    async function getShuffleActiveNodesPage() {
-      const res = await getShuffleActiveNodes()
-      pageData.tableData = res.data.data
-    }
-
-    // The system obtains data from global variables and requests the interface to obtain new data after data changes.
-    currentServerStore.$subscribe((mutable, state) => {
-      if (state.currentServer) {
-        getShuffleActiveNodesPage()
-      }
-    })
-
-    onMounted(() => {
-      // If the coordinator address to request is not found in the global variable, the request is not initiated.
-      if (currentServerStore.currentServer) {
-        getShuffleActiveNodesPage()
-      }
-    })
-
-    return { pageData, memFormatter, dateFormatter }
-  }
-}
-</script>

dashboard/src/main/webapp/src/pages/serverstatus/DecommissionednodeListPage.vue
@@ -1,106 +1,0 @@
-<!--
-  ~ Licensed to the Apache Software Foundation (ASF) under one or more
-  ~ contributor license agreements.  See the NOTICE file distributed with
-  ~ this work for additional information regarding copyright ownership.
-  ~ The ASF licenses this file to You under the Apache License, Version 2.0
-  ~ (the "License"); you may not use this file except in compliance with
-  ~ the License.  You may obtain a copy of the License at
-  ~
-  ~    http://www.apache.org/licenses/LICENSE-2.0
-  ~
-  ~ Unless required by applicable law or agreed to in writing, software
-  ~ distributed under the License is distributed on an "AS IS" BASIS,
-  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  ~ See the License for the specific language governing permissions and
-  ~ limitations under the License.
-  -->
-
-<template>
-  <div>
-    <el-table :data="pageData.tableData" height="550" style="width: 100%">
-      <el-table-column prop="id" label="Id" min-width="140" />
-      <el-table-column prop="ip" label="IP" min-width="80" />
-      <el-table-column prop="grpcPort" label="Port" min-width="80" />
-      <el-table-column prop="nettyPort" label="NettyPort" min-width="80" />
-      <el-table-column prop="usedMemory" label="UsedMem" min-width="80" :formatter="memFormatter" />
-      <el-table-column
-        prop="preAllocatedMemory"
-        label="PreAllocatedMem"
-        min-width="80"
-        :formatter="memFormatter"
-      />
-      <el-table-column
-        prop="availableMemory"
-        label="AvailableMem"
-        min-width="80"
-        :formatter="memFormatter"
-      />
-      <el-table-column prop="eventNumInFlush" label="FlushNum" min-width="80" />
-      <el-table-column prop="status" label="Status" min-width="80" />
-      <el-table-column
-          prop="registrationTime"
-          label="RegistrationTime"
-          min-width="80"
-          :formatter="dateFormatter"
-      />
-      <el-table-column
-        prop="timestamp"
-        label="HeartbeatTime"
-        min-width="80"
-        :formatter="dateFormatter"
-      />
-      <el-table-column prop="tags" label="Tags" min-width="80" />
-    </el-table>
-  </div>
-</template>
-<script>
-import { onMounted, reactive } from 'vue'
-import { getShuffleDecommissionedList } from '@/api/api'
-import { memFormatter, dateFormatter } from '@/utils/common'
-import { useCurrentServerStore } from '@/store/useCurrentServerStore'
-
-export default {
-  setup() {
-    const pageData = reactive({
-      tableData: [
-        {
-          id: '',
-          ip: '',
-          grpcPort: 0,
-          nettyPort: 0,
-          usedMemory: 0,
-          preAllocatedMemory: 0,
-          availableMemory: 0,
-          eventNumInFlush: 0,
-          tags: '',
-          status: '',
-          registrationTime: '',
-          timestamp: ''
-        }
-      ]
-    })
-    const currentServerStore = useCurrentServerStore()
-
-    async function getShuffleDecommissionedListPage() {
-      const res = await getShuffleDecommissionedList()
-      pageData.tableData = res.data.data
-    }
-
-    // The system obtains data from global variables and requests the interface to obtain new data after data changes.
-    currentServerStore.$subscribe((mutable, state) => {
-      if (state.currentServer) {
-        getShuffleDecommissionedListPage()
-      }
-    })
-
-    onMounted(() => {
-      // If the coordinator address to request is not found in the global variable, the request is not initiated.
-      if (currentServerStore.currentServer) {
-        getShuffleDecommissionedListPage()
-      }
-    })
-
-    return { pageData, memFormatter, dateFormatter }
-  }
-}
-</script>

dashboard/src/main/webapp/src/pages/serverstatus/DecommissioningNodeListPage.vue
@@ -1,106 +1,0 @@
-<!--
-  ~ Licensed to the Apache Software Foundation (ASF) under one or more
-  ~ contributor license agreements.  See the NOTICE file distributed with
-  ~ this work for additional information regarding copyright ownership.
-  ~ The ASF licenses this file to You under the Apache License, Version 2.0
-  ~ (the "License"); you may not use this file except in compliance with
-  ~ the License.  You may obtain a copy of the License at
-  ~
-  ~    http://www.apache.org/licenses/LICENSE-2.0
-  ~
-  ~ Unless required by applicable law or agreed to in writing, software
-  ~ distributed under the License is distributed on an "AS IS" BASIS,
-  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  ~ See the License for the specific language governing permissions and
-  ~ limitations under the License.
-  -->
-
-<template>
-  <div>
-    <el-table :data="pageData.tableData" height="550" style="width: 100%">
-      <el-table-column prop="id" label="Id" min-width="140" />
-      <el-table-column prop="ip" label="IP" min-width="80" />
-      <el-table-column prop="grpcPort" label="Port" min-width="80" />
-      <el-table-column prop="nettyPort" label="NettyPort" min-width="80" />
-      <el-table-column prop="usedMemory" label="UsedMem" min-width="80" :formatter="memFormatter" />
-      <el-table-column
-        prop="preAllocatedMemory"
-        label="PreAllocatedMem"
-        min-width="80"
-        :formatter="memFormatter"
-      />
-      <el-table-column
-        prop="availableMemory"
-        label="AvailableMem"
-        min-width="80"
-        :formatter="memFormatter"
-      />
-      <el-table-column prop="eventNumInFlush" label="FlushNum" min-width="80" />
-      <el-table-column prop="status" label="Status" min-width="80" />
-      <el-table-column
-          prop="registrationTime"
-          label="RegistrationTime"
-          min-width="80"
-          :formatter="dateFormatter"
-      />
-      <el-table-column
-        prop="timestamp"
-        label="HeartbeatTime"
-        min-width="80"
-        :formatter="dateFormatter"
-      />
-      <el-table-column prop="tags" label="Tags" min-width="80" />
-    </el-table>
-  </div>
-</template>
-<script>
-import { onMounted, reactive } from 'vue'
-import { getShuffleDecommissioningList } from '@/api/api'
-import { memFormatter, dateFormatter } from '@/utils/common'
-import { useCurrentServerStore } from '@/store/useCurrentServerStore'
-
-export default {
-  setup() {
-    const pageData = reactive({
-      tableData: [
-        {
-          id: '',
-          ip: '',
-          grpcPort: 0,
-          nettyPort: 0,
-          usedMemory: 0,
-          preAllocatedMemory: 0,
-          availableMemory: 0,
-          eventNumInFlush: 0,
-          tags: '',
-          status: '',
-          registrationTime: '',
-          timestamp: ''
-        }
-      ]
-    })
-    const currentServerStore = useCurrentServerStore()
-
-    async function getShuffleDecommissioningListPage() {
-      const res = await getShuffleDecommissioningList()
-      pageData.tableData = res.data.data
-    }
-
-    // The system obtains data from global variables and requests the interface to obtain new data after data changes.
-    currentServerStore.$subscribe((mutable, state) => {
-      if (state.currentServer) {
-        getShuffleDecommissioningListPage()
-      }
-    })
-
-    onMounted(() => {
-      // If the coordinator address to request is not found in the global variable, the request is not initiated.
-      if (currentServerStore.currentServer) {
-        getShuffleDecommissioningListPage()
-      }
-    })
-
-    return { pageData, memFormatter, dateFormatter }
-  }
-}
-</script>

dashboard/src/main/webapp/src/pages/serverstatus/ExcludeNodeList.vue
@@ -17,8 +17,14 @@
 
 <template>
   <div>
-    <el-table :data="pageData.tableData" height="550" style="width: 100%">
-      <el-table-column prop="id" label="ExcludeNodeId" min-width="180" />
+    <el-table
+      :data="pageData.tableData"
+      height="550"
+      style="width: 100%"
+      :default-sort="sortColumn"
+      @sort-change="sortChangeEvent"
+    >
+      <el-table-column prop="id" label="ExcludeNodeId" min-width="180" :sortable="true" />
     </el-table>
   </div>
 </template>
@@ -57,7 +63,15 @@ export default {
       }
     })
 
-    return { pageData }
+    const sortColumn = reactive({})
+    const sortChangeEvent = (sortInfo) => {
+      for (const sortColumnKey in sortColumn) {
+        delete sortColumn[sortColumnKey]
+      }
+      sortColumn[sortInfo.prop] = sortInfo.order
+    }
+
+    return { pageData, sortColumn, sortChangeEvent }
   }
 }
 </script>

dashboard/src/main/webapp/src/pages/serverstatus/LostNodeList.vue
@@ -1,107 +1,0 @@
-<!--
-  ~ Licensed to the Apache Software Foundation (ASF) under one or more
-  ~ contributor license agreements.  See the NOTICE file distributed with
-  ~ this work for additional information regarding copyright ownership.
-  ~ The ASF licenses this file to You under the Apache License, Version 2.0
-  ~ (the "License"); you may not use this file except in compliance with
-  ~ the License.  You may obtain a copy of the License at
-  ~
-  ~    http://www.apache.org/licenses/LICENSE-2.0
-  ~
-  ~ Unless required by applicable law or agreed to in writing, software
-  ~ distributed under the License is distributed on an "AS IS" BASIS,
-  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  ~ See the License for the specific language governing permissions and
-  ~ limitations under the License.
-  -->
-
-<template>
-  <div>
-    <el-table :data="pageData.tableData" height="550" style="width: 100%">
-      <el-table-column prop="id" label="Id" min-width="140" />
-      <el-table-column prop="ip" label="IP" min-width="80" />
-      <el-table-column prop="grpcPort" label="Port" min-width="80" />
-      <el-table-column prop="nettyPort" label="NettyPort" min-width="80" />
-      <el-table-column prop="usedMemory" label="UsedMem" min-width="80" :formatter="memFormatter" />
-      <el-table-column
-        prop="preAllocatedMemory"
-        label="PreAllocatedMem"
-        min-width="80"
-        :formatter="memFormatter"
-      />
-      <el-table-column
-        prop="availableMemory"
-        label="AvailableMem"
-        min-width="80"
-        :formatter="memFormatter"
-      />
-      <el-table-column prop="eventNumInFlush" label="FlushNum" min-width="80" />
-      <el-table-column prop="status" label="Status" min-width="80" />
-      <el-table-column
-          prop="registrationTime"
-          label="RegistrationTime"
-          min-width="80"
-          :formatter="dateFormatter"
-      />
-      <el-table-column
-        prop="timestamp"
-        label="HeartbeatTime"
-        min-width="80"
-        :formatter="dateFormatter"
-      />
-      <el-table-column prop="tags" label="Tags" min-width="80" />
-    </el-table>
-  </div>
-</template>
-<script>
-import { onMounted, reactive } from 'vue'
-import { getShuffleLostList } from '@/api/api'
-import { memFormatter, dateFormatter } from '@/utils/common'
-import { useCurrentServerStore } from '@/store/useCurrentServerStore'
-
-export default {
-  setup() {
-    const pageData = reactive({
-      tableData: [
-        {
-          id: '',
-          ip: '',
-          grpcPort: 0,
-          nettyPort: 0,
-          usedMemory: 0,
-          preAllocatedMemory: 0,
-          availableMemory: 0,
-          eventNumInFlush: 0,
-          tags: '',
-          status: '',
-          registrationTime: '',
-          timestamp: ''
-        }
-      ]
-    })
-
-    const currentServerStore = useCurrentServerStore()
-
-    async function getShuffleLostListPage() {
-      const res = await getShuffleLostList()
-      pageData.tableData = res.data.data
-    }
-
-    // The system obtains data from global variables and requests the interface to obtain new data after data changes.
-    currentServerStore.$subscribe((mutable, state) => {
-      if (state.currentServer) {
-        getShuffleLostListPage()
-      }
-    })
-
-    onMounted(() => {
-      // If the coordinator address to request is not found in the global variable, the request is not initiated.
-      if (currentServerStore.currentServer) {
-        getShuffleLostListPage()
-      }
-    })
-
-    return { pageData, memFormatter, dateFormatter }
-  }
-}
-</script>

dashboard/src/main/webapp/src/pages/serverstatus/NodeListPage.vue
@@ -1,0 +1,169 @@
+<!--
+  ~ Licensed to the Apache Software Foundation (ASF) under one or more
+  ~ contributor license agreements.  See the NOTICE file distributed with
+  ~ this work for additional information regarding copyright ownership.
+  ~ The ASF licenses this file to You under the Apache License, Version 2.0
+  ~ (the "License"); you may not use this file except in compliance with
+  ~ the License.  You may obtain a copy of the License at
+  ~
+  ~    http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing, software
+  ~ distributed under the License is distributed on an "AS IS" BASIS,
+  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  ~ See the License for the specific language governing permissions and
+  ~ limitations under the License.
+  -->
+
+<template>
+  <div>
+    <el-table
+      :data="listPageData.tableData"
+      height="550"
+      style="width: 100%"
+      :default-sort="sortColumn"
+      @sort-change="sortChangeEvent"
+    >
+      <el-table-column prop="id" label="Id" min-width="140" sortable fixed />
+      <el-table-column prop="ip" label="IP" min-width="80" sortable />
+      <el-table-column prop="grpcPort" label="Port" min-width="80" />
+      <el-table-column prop="nettyPort" label="NettyPort" min-width="80" />
+      <el-table-column
+        prop="usedMemory"
+        label="UsedMem"
+        min-width="80"
+        :formatter="memFormatter"
+        sortable
+      />
+      <el-table-column
+        prop="preAllocatedMemory"
+        label="PreAllocatedMem"
+        min-width="100"
+        :formatter="memFormatter"
+        sortable
+      />
+      <el-table-column
+        prop="availableMemory"
+        label="AvailableMem"
+        min-width="80"
+        :formatter="memFormatter"
+        sortable
+      />
+      <el-table-column prop="eventNumInFlush" label="FlushNum" min-width="80" sortable />
+      <el-table-column prop="status" label="Status" min-width="80" sortable />
+      <el-table-column
+        prop="registrationTime"
+        label="RegistrationTime"
+        min-width="120"
+        :formatter="dateFormatter"
+      />
+      <el-table-column
+        prop="timestamp"
+        label="HeartbeatTime"
+        min-width="120"
+        :formatter="dateFormatter"
+        sortable
+      />
+      <el-table-column prop="tags" label="Tags" min-width="140" />
+    </el-table>
+  </div>
+</template>
+<script>
+import { onMounted, reactive, watch } from 'vue'
+import { memFormatter, dateFormatter } from '@/utils/common'
+import {
+  getShuffleActiveNodes,
+  getShuffleDecommissionedList,
+  getShuffleDecommissioningList,
+  getShuffleLostList,
+  getShuffleUnhealthyList
+} from '@/api/api'
+import { useRouter } from 'vue-router'
+import { useCurrentServerStore } from '@/store/useCurrentServerStore'
+
+export default {
+  setup() {
+    const router = useRouter()
+    const currentServerStore = useCurrentServerStore()
+    const sortColumn = reactive({})
+    const listPageData = reactive({
+      tableData: [
+        {
+          id: '',
+          ip: '',
+          grpcPort: 0,
+          nettyPort: 0,
+          usedMemory: 0,
+          preAllocatedMemory: 0,
+          availableMemory: 0,
+          eventNumInFlush: 0,
+          tags: '',
+          status: '',
+          registrationTime: '',
+          timestamp: ''
+        }
+      ]
+    })
+
+    async function getShuffleActiveNodesPage() {
+      const res = await getShuffleActiveNodes()
+      listPageData.tableData = res.data.data
+    }
+
+    async function getShuffleDecommissionedListPage() {
+      const res = await getShuffleDecommissionedList()
+      listPageData.tableData = res.data.data
+    }
+
+    async function getShuffleDecommissioningListPage() {
+      const res = await getShuffleDecommissioningList()
+      listPageData.tableData = res.data.data
+    }
+
+    async function getShuffleLostListPage() {
+      const res = await getShuffleLostList()
+      listPageData.tableData = res.data.data
+    }
+
+    async function getShuffleUnhealthyListPage() {
+      const res = await getShuffleUnhealthyList()
+      listPageData.tableData = res.data.data
+    }
+
+    const loadPageData = () => {
+      if (router.currentRoute.value.name === 'activeNodeList') {
+        getShuffleActiveNodesPage()
+      } else if (router.currentRoute.value.name === 'decommissioningNodeList') {
+        getShuffleDecommissioningListPage()
+      } else if (router.currentRoute.value.name === 'decommissionedNodeList') {
+        getShuffleDecommissionedListPage()
+      } else if (router.currentRoute.value.name === 'unhealthyNodeList') {
+        getShuffleUnhealthyListPage()
+      } else if (router.currentRoute.value.name === 'lostNodeList') {
+        getShuffleLostListPage()
+      }
+    }
+
+    onMounted(() => {
+      // If the coordinator address to request is not found in the global variable, the request is not initiated.
+      if (currentServerStore.currentServer) {
+        loadPageData()
+      }
+    })
+
+    watch(router.currentRoute, () => {
+      if (currentServerStore.currentServer) {
+        loadPageData()
+      }
+    })
+
+    const sortChangeEvent = (sortInfo) => {
+      for (const sortColumnKey in sortColumn) {
+        delete sortColumn[sortColumnKey]
+      }
+      sortColumn[sortInfo.prop] = sortInfo.order
+    }
+    return { listPageData, sortColumn, sortChangeEvent, memFormatter, dateFormatter }
+  }
+}
+</script>

commit e0ce6a02f4065d71d28a825290dc0a4eff19238a

[FOLLOWUP] [#1401] Fix(dashboard): Fix the display of app reg time on app page (#1963)
### What changes were proposed in this pull request?

Display app reg time in dashboard.

### Why are the changes needed?

Fix typo error introduced by #1868 

### Does this PR introduce _any_ user-facing change?

User can see app reg time from dashboard.

### How was this patch tested?

Open a dashboard ui and submit an application and check the app reg time.

dashboard/src/main/webapp/src/pages/ApplicationPage.vue
@@ -55,7 +55,7 @@
       >
         <el-table-column prop="appId" label="AppId" min-width="180" sortable fixed />
         <el-table-column prop="userName" label="UserName" min-width="180" sortable />
-        <el-table-columnsortable
+        <el-table-column
           prop="registrationTime"
           label="Registration Time"
           min-width="180"