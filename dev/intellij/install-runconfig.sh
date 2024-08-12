ce20683ad1a6eb0d55a6f91b0c7c80b88db24259
maobaolong <baoloongmao@tencent.com>
2024-08-12 10:22:31 +0800

    [MINOR] Improve(script): Change intellij launch config file name and local-dev conf folder (#2008)
    ### What changes were proposed in this pull request?
    
    - Copy local_dev conf to .idea folder which a folder in `.gitignore`, so, local_dev conf changes are not tracked by git.
    - Remove the xml suffix of `runConfigurations/xxx.xml`, if not, intellij idea will find it and load the configurations into idea, after that, any idea launch configuration change will persist to these files, what is not we want. Remove the xml suffix can make idea don't treat these files as launch configuration files.
    
    ### Why are the changes needed?
    
    Without this change, any local purpose change will tracked by git, this make contributors busy to revert changes or push local changes to git repository by accident.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Just run the `incubator-uniffle/dev/intellij/install-runconfig.sh`

--- dev/intellij/install-runconfig.sh
+++ dev/intellij/install-runconfig.sh
@@ -22,7 +22,12 @@ set -o nounset   # exit the script if you try to use an uninitialised variable
 set -o errexit   # exit the script if any statement returns a non-true return value
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-SRC_DIR="$SCRIPT_DIR/runConfigurations"
-DEST_DIR="$SCRIPT_DIR/../../.idea/runConfigurations/"
+SRC_DIR="$SCRIPT_DIR/runConfs"
+DEST_DIR="$SCRIPT_DIR/../../.idea/runConfigurations1/"
+mkdir -p "$DEST_DIR"
+ls -1 "$SRC_DIR" | xargs -n1 -I FILE cp "$SRC_DIR/FILE" "$DEST_DIR/FILE.xml"
+
+SRC_DIR="$SCRIPT_DIR/../../dev/local_dev_template"
+DEST_DIR="$SCRIPT_DIR/../../.idea/local_dev1/"
 mkdir -p "$DEST_DIR"
 ls -1 "$SRC_DIR" | xargs -n1 -I FILE cp "$SRC_DIR/FILE" "$DEST_DIR"
