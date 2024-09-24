4bb1f3ec704566b45c212d66eeb3881fd89da58c
zhengchenyu <zhengchenyu16@163.com>
2024-09-24 21:59:37 +0800

    add comment

--- server/src/main/java/org/apache/uniffle/server/buffer/ShuffleBufferWithSkipList.java
+++ server/src/main/java/org/apache/uniffle/server/buffer/ShuffleBufferWithSkipList.java
@@ -62,6 +62,8 @@ public class ShuffleBufferWithSkipList extends AbstractShuffleBuffer {
 
     synchronized (this) {
       for (ShufflePartitionedBlock block : data.getBlockList()) {
+        // If sendShuffleData retried, we may receive duplicate block. The duplicate
+        // block would gc without release. Here we must release the duplicated block.
         if (!blocksMap.containsKey(block.getBlockId())) {
           blocksMap.put(block.getBlockId(), block);
           blockCount++;
