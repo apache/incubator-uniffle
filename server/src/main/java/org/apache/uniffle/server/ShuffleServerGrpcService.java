886bd76195ef3bd0a817a001e5c99edf813bcaa8
baoloongmao <baoloongmao@tencent.com>
2024-10-22 20:19:08 +0800

    Server heartbeat to coordinator while unregister shuffle

--- server/src/main/java/org/apache/uniffle/server/ShuffleServerGrpcService.java
+++ server/src/main/java/org/apache/uniffle/server/ShuffleServerGrpcService.java
@@ -162,6 +162,7 @@ public class ShuffleServerGrpcService extends ShuffleServerImplBase {
       }
       String responseMessage = "OK";
       try {
+        shuffleServer.sendHeartbeat();
         shuffleServer.getShuffleTaskManager().removeShuffleDataAsync(appId);
         if (shuffleServer.isRemoteMergeEnable()) {
           shuffleServer.getShuffleTaskManager().removeShuffleDataAsync(appId + MERGE_APP_SUFFIX);
@@ -203,6 +204,7 @@ public class ShuffleServerGrpcService extends ShuffleServerImplBase {
       }
       String responseMessage = "OK";
       try {
+        shuffleServer.sendHeartbeat();
         shuffleServer.getShuffleTaskManager().removeShuffleDataAsync(appId, shuffleId);
         if (shuffleServer.isRemoteMergeEnable()) {
           shuffleServer
