949d57f501034e9862493fe7ece5cc5d9d50d4b8
baoloongmao <baoloongmao@tencent.com>
2024-11-06 10:15:54 +0800

    Add a UT splitPartitionTest

--- client-spark/common/src/test/java/org/apache/spark/shuffle/handle/MutableShuffleHandleInfoTest.java
+++ client-spark/common/src/test/java/org/apache/spark/shuffle/handle/MutableShuffleHandleInfoTest.java
@@ -20,6 +20,7 @@ package org.apache.spark.shuffle.handle;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -141,5 +142,66 @@ public class MutableShuffleHandleInfoTest {
     assertEquals(a, inventory.get(1).get(0).get(0));
     assertEquals(b, inventory.get(1).get(1).get(0));
     assertEquals(c, inventory.get(2).get(0).get(0));
+  }
+
+  @Test
+  public void testUpdateAssignmentOnPartitionSplit() {
+    Map<Integer, List<ShuffleServerInfo>> partitionToServers = new HashMap<>();
+    partitionToServers.put(1, Arrays.asList(createFakeServerInfo("a"), createFakeServerInfo("b")));
+    partitionToServers.put(2, Arrays.asList(createFakeServerInfo("c")));
+
+    MutableShuffleHandleInfo handleInfo =
+        new MutableShuffleHandleInfo(1, partitionToServers, new RemoteStorageInfo(""));
+
+    // case1: update the replacement servers but has existing servers
+    Set<ShuffleServerInfo> updated =
+        handleInfo.updateAssignment(
+            1, "a", Sets.newHashSet(createFakeServerInfo("a"), createFakeServerInfo("d")));
+    assertTrue(updated.stream().findFirst().get().getId().equals("d"));
+
+    // case2: update when having multiple servers
+    Map<Integer, Map<Integer, List<ShuffleServerInfo>>> partitionReplicaAssignedServers =
+        new HashMap<>();
+    List<ShuffleServerInfo> servers =
+        new ArrayList<>(
+            Arrays.asList(
+                createFakeServerInfo("a"),
+                createFakeServerInfo("b"),
+                createFakeServerInfo("c"),
+                createFakeServerInfo("d")));
+    partitionReplicaAssignedServers
+        .computeIfAbsent(1, x -> new HashMap<>())
+        .computeIfAbsent(0, x -> servers);
+    handleInfo =
+        new MutableShuffleHandleInfo(1, new RemoteStorageInfo(""), partitionReplicaAssignedServers);
+
+    Map<Integer, List<ShuffleServerInfo>> availablePartitionServers =
+        handleInfo.getAvailablePartitionServersForWriter();
+    assertEquals("d", availablePartitionServers.get(1).get(0).getHost());
+    Map<Integer, List<ShuffleServerInfo>> assignment = handleInfo.getAllPartitionServersForReader();
+    assertEquals(4, assignment.get(1).size());
+
+    int partitionId = 1;
+
+    handleInfo.getReplacementsForPartition(1, "a");
+    HashSet<ShuffleServerInfo> replacements =
+        Sets.newHashSet(
+            createFakeServerInfo("b"),
+            createFakeServerInfo("d"),
+            createFakeServerInfo("e"),
+            createFakeServerInfo("f"));
+    updated = handleInfo.updateAssignmentOnPartitionSplit(partitionId, "a", replacements);
+    assertEquals(updated, Sets.newHashSet(createFakeServerInfo("e"), createFakeServerInfo("f")));
+
+    Set<String> excludedServers = handleInfo.listExcludedServersForPartition(partitionId);
+    assertEquals(1, excludedServers.size());
+    assertEquals("a", excludedServers.iterator().next());
+    assertEquals(replacements, handleInfo.getReplacementsForPartition(1, "a"));
+    availablePartitionServers = handleInfo.getAvailablePartitionServersForWriter();
+    // The current writer is the last one
+    assertEquals("f", availablePartitionServers.get(1).get(0).getHost());
+    assignment = handleInfo.getAllPartitionServersForReader();
+    // All the servers were selected as writer are available as reader
+    assertEquals(6, assignment.get(1).size());
   }
 }
