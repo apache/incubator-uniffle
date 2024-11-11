4db95c3c883b338076d6ccaacd2aca7268e222e6
kqhzz <kuangqhzz@gmail.com>
2024-11-11 20:00:09 +0800

    [#2152] improvement(dashboard): Add session cache to support refresh page of dashboard (#2238)
    ### Why are the changes needed?
    
    Fix: #2152
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.

--- dashboard/src/main/webapp/src/components/LayoutPage.vue
+++ dashboard/src/main/webapp/src/components/LayoutPage.vue
@@ -109,18 +109,21 @@ export default {
       }
     }
 
-    function handleChangeServer(key) {
+    const handleChangeServer = (key) => {
       currentServerStore.currentServer = key
     }
 
     async function getSelectCurrentServer() {
       const res = await getAllCoordinatorAddrees()
       const selectCurrentServer = res.data.data
-      currentServerStore.currentServer = Object.keys(selectCurrentServer)[0]
+      if (!currentServerStore.currentServer) {
+        currentServerStore.currentServer = Object.keys(selectCurrentServer)[0]
+      }
       hostNameAndPorts.length = 0
       Object.entries(selectCurrentServer).forEach(([key, value]) => {
         hostNameAndPorts.push({ value: value, label: key })
       })
+      hostNameAndPorts.sort((a, b) => a.label.localeCompare(b.label))
     }
 
     onMounted(() => {
